5cbcb3076eea8c9da14b46d4d92622356bab26f1
AyushUprety <ayushuprety1@gmail.com>
2022-08-15 16:38:57 +0545

    Created header and footer componenet and installed it in app.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Box from "@mui/material/Box";
+import Navbar from "./components/navbar";
+import Footer from "./components/footer";
+import Home from "./pages/home";
 
 const App = () => {
   return (
     <Box>
-      <navbar />
+      <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/exercise/:id" element={<exerciseDetail />} />
       </Routes>
+      <Footer />
     </Box>
   );
 };
