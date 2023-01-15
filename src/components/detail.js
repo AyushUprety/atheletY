745828f2c01d74fe5c9e5353cab1f00cfb96f78e
AyushUprety <ayushuprety1@gmail.com>
2023-01-15 22:17:04 +0545

    Added logic to fetch detail about a particular exercise

--- src/components/detail.js
+++ src/components/detail.js
@@ -1,7 +1,20 @@
 import React from "react"
+import { Stack } from "@mui/system"
+import { Typography } from "@mui/material"
 
 const Detail = ({ icon }) => {
-  return <div>{icon ? <img src={icon} /> : ""}</div>
+  return (
+    <div>
+      <Stack direction="row">
+        {icon ? <img src={icon} /> : ""}
+        <Typography variant="h7">
+          Exercise can help you live healthy and feel dandy.thisexercise is one
+          of the best exercise for target.It will help you grow them muscles in
+          your target.
+        </Typography>
+      </Stack>
+    </div>
+  )
 }
 
 export default Detail
--- src/pages/ExerciseDetail.js
+++ src/pages/ExerciseDetail.js
@@ -8,21 +8,21 @@ import { useParams } from "react-router-dom"
 import { useState } from "react"
 
 const ExerciseDetail = () => {
-  const { id } = useParams()
-  const [icon, setIcon] = useState("")
+  const { id } = useParams() // useEffect use na garda infinte loop ma gako thiyo yo code.What happened was first fetchdata was executed.At execution it changed the state.Now, when state is changed component rerenders.When component is rendered again fetchdata function is called which again changed state due to which component is rendered agian.So first ma null state after this function runs.state value is updated.Now,again from top to bottom code is executed.As a result state again becomes null. This process continues infinetely.
+  const [data, setData] = useState("")
   const fetchdata = async () => {
     const response = await exerciseData(
       `https://exercisedb.p.rapidapi.com/exercises/exercise/${id}`,
       exerciseoptions
     )
     console.log(response)
-    setIcon(response.gifUrl)
+    setData(response)
   }
   fetchdata()
 
   return (
     <div>
-      <Detail icon={icon} />
+      <Detail name={data.name} icon={data.gifUrl} />
       <ExerciseVideos />
       <SimiliarPart />
     </div>
